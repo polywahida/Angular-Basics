acb8e414bf223d0cec25b6f5ebeedf3d434cce3c
poly_wahida <ifrahpoly@gmail.com>
2023-08-27 03:40:28 +0600

    Angular basics part-2

--- my-first-project/src/app/app.module.ts
+++ my-first-project/src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { BindingComponent } from './pages/binding/binding.component';
+import { ParentComponent } from './pages/binding/parent/parent.component';
+import { ChildComponent } from './pages/binding/child/child.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    DashboardComponent,
+    BindingComponent,
+    ParentComponent,
+    ChildComponent
   ],
   imports: [
-    BrowserModule
+    FormsModule,
+    BrowserModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
